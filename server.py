c40e265a6c4e818209301026dc5fab8de790ccc8
Maxouille <xd.maxouille@gmail.com>
2022-07-11 21:28:49 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,2 +1,2 @@
-import urllib.request, jsongithub_link = "https://github_username.github.io/json_file.json"with urllib.request.urlopen(github_link) as url: 
+import urllib.request, jsongithub_link = "https://github.com/Maxouille64/database/blob/main/teams.json" with urllib.request.urlopen(github_link) as url: 
    json_data = json.loads(url.read().decode())print(json_data)
