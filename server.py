597754311a8b878943fd4ddfebb3359602a0f356
Maxouille <xd.maxouille@gmail.com>
2022-07-11 21:38:58 +0200

    Delete server.py

--- server.py
+++
@@ -1,4 +0,0 @@
-import urllib.request, json
-github_link = "https://github.com/Maxouille64/database/blob/main/teams.json" 
-with urllib.request.urlopen(github_link) as url: 
-   json_data = json.loads(url.read().decode())print(json_data)
